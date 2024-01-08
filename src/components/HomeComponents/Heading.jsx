ef4010ef841c02bd3a5d83b0e48f789c831a6684
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-08 12:58:10 +0000

    styling for lg font removed

--- src/components/HomeComponents/Heading.jsx
+++ src/components/HomeComponents/Heading.jsx
@@ -3,9 +3,7 @@ import React from "react";
 export default function Heading() {
   return (
     <div>
-      <h1 className="header__title text-5xl font-black  md:text-yellow-500">
-        Maxwell Duedu
-      </h1>
+      <h1 className="header__title text-5xl font-black ">Maxwell Duedu</h1>
     </div>
   );
 }
