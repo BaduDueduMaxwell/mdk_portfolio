1dfa95892c6870b175aa98c7eb20ce21ed16b221
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-08 13:31:12 +0000

    added zoom animation for blog page

--- src/components/BlogComponents/post1.jsx
+++ src/components/BlogComponents/post1.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 export default function Post1() {
   return (
     <div>
       <div className="px-2">
         <div className="flex -mx-2">
-          <div className="w-full sm:w-1/2 p-5">
+          <motion.div
+            className="w-full sm:w-1/2 p-5"
+            whileHover={{ scale: 1.1 }}
+          >
             <div className="sm:h-100">
               <a href="https://medium.com/@duedumaxwell43/understanding-python-if-and-else-statements-a-comprehensive-guide-efcc566e6cc0">
-              <img
+                <img
                   src="https://miro.medium.com/v2/resize:fit:1100/format:webp/1*0qa5w1wa0_L6tfoeA3tVPQ.jpeg"
                   alt=""
                 />
@@ -19,9 +23,9 @@ export default function Post1() {
                 </h5>
               </a>
             </div>
-          </div>
+          </motion.div>
           <div className="w-full sm:w-1/2 p-5">
-            <div className="sm:h-100">
+            <motion.div className="sm:h-100" whileHover={{ scale: 1.1 }}>
               <a href="https://medium.com/@duedumaxwell43/python-common-errors-part-1-c1e5a03c67d8">
                 <img
                   src="https://images.unsplash.com/photo-1526379095098-d400fd0bf935?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cHl0aG9ufGVufDB8fDB8fHww"
@@ -32,7 +36,7 @@ export default function Post1() {
                   Python Common Errors…(Part 1)
                 </h5>
               </a>
-            </div>
+            </motion.div>
           </div>
         </div>
       </div>
