b1779161cedb879b253b3785ba744d2cde9356dc
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-08 12:55:21 +0000

    added paragraph and syling for bullet class

--- src/components/AboutComponents/Paragraph.jsx
+++ src/components/AboutComponents/Paragraph.jsx
@@ -8,12 +8,14 @@ export default function Paragraph() {
         <a href="https://www.linkedin.com/in/maxwell-duedu-05a026199/">
           Maxwell Duedu.
         </a>
-        Depending on my mood I am a Software Engineer or a technical writer. I have
-        written a number of technical blogs in which I broke down complex topics
-        and defined some best practices to implement in code. As an engineer and
-        a technical writer, my goal is to develop tools of technological
-        advancements that not only solve current problems but also pave the way
-        for future breakthroughs.
+        Depending on my mood I am a Software Engineer or a technical writer. I
+        have written a number of technical blogs in which I broke down complex
+        topics and defined some best practices to implement in code. As an
+        engineer and a technical writer, my goal is to develop tools of
+        technological advancements that not only solve current problems but also
+        pave the way for future breakthroughs. When I'm not pushing codes and
+        writing blogs, you'll find me sharing my love of CSS on my YouTube,
+        reading a book or traveling to see new places.
       </p>
     </div>
   );
--- src/components/AboutComponents/Paragraph2.jsx
+++ src/components/AboutComponents/Paragraph2.jsx
@@ -4,7 +4,7 @@ export default function Paragraph2() {
   return (
     <div>
       <p>
-        Previously at my internships at{" "}
+        At my previous internships at
         <a href="https://www.wewireafrica.com/">Wewire Africa,</a>I helped build
         a cross-border payments platform. This platform aimed to address the
         longstanding challenge faced by individuals and businesses in Africa who
@@ -12,7 +12,17 @@ export default function Paragraph2() {
         and Europe; worked at
         <a href="https://www.ogateway.io">Ogateway Ghana Limited</a> to develop
         a payment collection methods tailored to suit businesses and needs.
+        <br />
       </p>
+      <div className="ml-12">
+        <h4 className="font-bold "> Random Facts About Me</h4>
+        <ul>
+          <li className="bullet">I drink a lot of tea</li>
+          <li className="bullet">I'm slightly addicted to Twitter</li>
+          <li className="bullet">Beach days are my zen time</li>
+          <li className="bullet">Love traveling to see new places</li>
+        </ul>
+      </div>
     </div>
   );
 }
