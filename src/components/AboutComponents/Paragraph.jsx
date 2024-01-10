15dac6ea7b525970b46727005dd6489997e77407
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-10 12:39:13 +0000

    made changes to the about

--- src/components/AboutComponents/Paragraph.jsx
+++ src/components/AboutComponents/Paragraph.jsx
@@ -8,10 +8,8 @@ export default function Paragraph() {
         <a href="https://www.linkedin.com/in/maxwell-duedu-05a026199/">
           Maxwell Duedu.
         </a>
-        Depending on my mood, I am a Software Engineer or a technical writer. I
-        have written a number of technical blogs in which I broke down complex
-        topics and defined some best practices to implement in code. As an
-        engineer and a technical writer, my goal is to develop tools of
+        I am a Software Engineer and a Technical Writer  based in Accra, Ghana. As
+        an engineer and a technical writer my goal is to develop tools of
         technological advancements that not only solve current problems but also
         pave the way for future breakthroughs. When I'm not pushing codes and
         writing blogs, you'll find me sharing my love of CSS on my
