cce362eb2c9ff2e39c74b7093c8791bc064cef66
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-10 12:56:10 +0000

    modified to bullet form

--- src/components/AboutComponents/Paragraph2.jsx
+++ src/components/AboutComponents/Paragraph2.jsx
@@ -3,17 +3,26 @@ import React from "react";
 export default function Paragraph2() {
   return (
     <div>
-      <p>
-        At my previous internships at
-        <a href="https://www.wewireafrica.com/">Wewire Africa,</a>I helped build
-        a cross-border payments platform. This platform aimed to address the
-        longstanding challenge faced by individuals and businesses in Africa who
-        seek to engage with tier-one countries like the United States, the UK,
-        and Europe; worked at
-        <a href="https://www.ogateway.io">Ogateway Ghana Limited</a> to develop
-        a payment collection methods tailored to suit businesses and needs.
-        <br />
-      </p>
+      <div className="ml-12">
+        <h4 className="font-bold "> Career</h4>
+        <ul>
+          <li className="bullet">
+            At my previous internships at
+            <a href="https://www.wewireafrica.com/">Wewire Africa,</a>I helped
+            build a cross-border payments platform. This platform aimed to
+            address the longstanding challenge faced by individuals and
+            businesses in Africa who seek to engage with tier-one countries like
+            the United States, the UK, and Europe;
+          </li>
+          <li className="bullet">
+            Worked at{" "}
+            <a href="https://www.ogateway.io">Ogateway Ghana Limited</a> to
+            develop a payment collection methods tailored to suit businesses and
+            needs.{" "}
+          </li>
+        </ul>
+      </div>
+      <br />
       <div className="ml-12">
         <h4 className="font-bold "> Fun Facts About Me</h4>
         <ul>
