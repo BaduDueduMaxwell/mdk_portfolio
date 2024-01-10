00e4dba9c28c1f6c710b1426977b1201257368e7
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-10 12:19:09 +0000

    added letter spacing and increase font size

--- src/components/HomeComponents/Heading.jsx
+++ src/components/HomeComponents/Heading.jsx
@@ -3,7 +3,9 @@ import React from "react";
 export default function Heading() {
   return (
     <div>
-      <h1 className="header__title text-5xl font-black ">Maxwell Duedu</h1>
+      <h1 className="header__title text-6xl font-black tracking-widest">
+        Maxwell Duedu
+      </h1>
     </div>
   );
 }
