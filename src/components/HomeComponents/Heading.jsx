53f545dbc252a3096d066e2a2ea0a4ff7ea14a51
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-01-10 12:20:27 +0000

    changed font size

--- src/components/HomeComponents/Heading.jsx
+++ src/components/HomeComponents/Heading.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Heading() {
   return (
     <div>
-      <h1 className="header__title text-6xl font-black tracking-widest">
+      <h1 className="header__title text-5xl font-black tracking-widest">
         Maxwell Duedu
       </h1>
     </div>
