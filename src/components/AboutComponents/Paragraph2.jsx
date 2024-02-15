87f7491aca688b833ce860f3fa6e7c7c6605f361
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-02-15 20:37:06 +0000

    about:added experience during internships

--- src/components/AboutComponents/Paragraph2.jsx
+++ src/components/AboutComponents/Paragraph2.jsx
@@ -14,12 +14,46 @@ export default function Paragraph2() {
             businesses in Africa who seek to engage with tier-one countries like
             the United States, the UK, and Europe;
           </li>
+          <div className="m-10">
+            <h5 className="text-1xl mt-4 b-4">Experience:</h5>
+            <ul>
+              <li className="mb-4 font-bold">
+                - Created a clean and responsive website using TypeScript and
+                React, leading to increased traffic and engagement by 20%.
+              </li>
+              <li className="mb-4 font-bold">
+                - Engineered robust quality assurance frameworks, incorporating
+                diverse testing methods and user acceptance testing, leading to
+                significant improvements in frontend code quality and
+                reliability.
+              </li>
+            </ul>
+          </div>
           <li className="bullet">
             Worked at{" "}
             <a href="https://www.ogateway.io">Ogateway Ghana Limited</a> to
             develop a payment collection methods tailored to suit businesses and
             needs.{" "}
           </li>
+          <div className="m-10">
+            <h5 className="text-1xl mt-4 b-4">Experience:</h5>
+            <ul>
+              <li className="mb-4 font-bold">
+                - Implemented a robust Time-based OTP system integrated with
+                email delivery and push notifications which decreased fraudulent
+                login by 75%.
+              </li>
+              <li className="mb-4 font-bold">
+                - Collaborated with UI/UX designers to develop a user–friendly
+                OTP input field prototype, achieving a 99% success rate of entry
+                across desktop, mobile, and tablet devices.
+              </li>
+              <li className="mb-4 font-bold">
+                - Contributed to cross border transactions testings and
+                participated in team meetings, scrum sessions, and code reviews.
+              </li>
+            </ul>
+          </div>
         </ul>
       </div>
       <br />
