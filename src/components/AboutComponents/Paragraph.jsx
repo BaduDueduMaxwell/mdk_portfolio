d37824b543a2ab509bf66338cd54c02d5ff892c9
Badu Duedu Maxwell <duedumaxwell43@gmail.com>
2024-02-15 20:25:45 +0000

    about: edited summary

--- src/components/AboutComponents/Paragraph.jsx
+++ src/components/AboutComponents/Paragraph.jsx
@@ -5,14 +5,17 @@ export default function Paragraph() {
     <div>
       <p>
         Welcome to my corner of the web! I am
-        <a href="https://www.linkedin.com/in/maxwellduedu/">
-          Maxwell Duedu.
-        </a>
-        I am a Software Engineer and a Technical Writer  based in Accra, Ghana. As
-        an engineer and a technical writer my goal is to develop tools of
-        technological advancements that not only solve current problems but also
-        pave the way for future breakthroughs. When I'm not pushing codes and
-        writing blogs, you'll find me sharing my love of CSS on my
+        <a href="https://www.linkedin.com/in/maxwellduedu/">Maxwell Duedu.</a>I
+        am a motivated Computer Science student at Kibo School of Technology,
+        eager to utilize my JavaScript, Python, TypeScript, and React skills to
+        empower social good initiatives. With a year's experience in designing
+        and maintaining front-end web applications, I've achieved measurable
+        results, such as increasing page load speeds by 20% and boosting website
+        conversion rates. Additionally, I implemented a front-end feature
+        integrated with email delivery notification, reducing fraudulent logins
+        by 75%. I thrive in collaborating with cross-functional teams and
+        delivering projects on time. When I'm not pushing codes and writing
+        blogs, you'll find me sharing my love of CSS on my
         <a
           href="https://www.youtube.com/channel/UCqXbYD4fConbn5Ap-p_eA1w"
           target="_blank"
